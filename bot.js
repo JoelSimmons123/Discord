d590fc28b6c7476d23355bb642159db1deef0a92
JoelSimmons123 <47631457+JoelSimmons123@users.noreply.github.com>
2019-04-22 22:54:01 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on("message", message => {
+    if(message.channel.id !== "569880248980865057" || message.member.hasPermission('ADMINISTRATOR') || message.member.roles.has("569882837373812768")) return;
+    message.delete(1000*60*1);
+});
+
+client.on("disconnect", () => {
+         console.log(`You have been disconnected`);
+});
+client.on("error", () => {
+    console.error();
+});
+client.on("reconnecting", () => {
+    console.log("You have been reconnected");
+});
+
+client.login(process.env.BOT_TOKEN);
